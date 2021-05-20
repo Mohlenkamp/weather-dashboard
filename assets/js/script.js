7a844f9693fbbef9ec8da07acd827e68ec65713b
John Mohlenkamp <john@mohlenkamp.com>
2021-05-20 17:38:33 -0500

    Finished styling and added icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,40 @@ var todayDate = ("(" + (today.getMonth()+1) + "/" + today.getDate() + "/" + toda
 function setIconAndColor(data){
     // This is setting the little icon to the right of the city name and date
     // as well as the color box around the UV Index value
+    // UV Color coding from:
+    // https://wp02-media.cdn.ihealthspot.com/wp-content/uploads/sites/200/2018/08/03014643/UV-Index.png
+
     var todayUV = document.querySelector("#todayUV")
     var todayIcon = document.querySelector("#todayIcon")
-
-    
+    var iconID = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png"
+    todayIcon.setAttribute("src", iconID)
+    switch (parseInt(data.current.uvi)){
+        case 0:
+        case 1:
+        case 2:
+                todayUV.classList.add("uvi-low")
+                break;
+        case 3: 
+        case 4:
+        case 5:
+                todayUV.classList.add("uvi-med")
+                break;
+        case 6:
+        case 7: 
+                todayUV.classList.add("uvi-high")
+                break;
+        case 8:
+        case 9:
+        case 10:
+                todayUV.classList.add("uvi-very-high")
+                break;      
+        case 11:
+        default:
+                todayUV.classList.add("uvi-extremely-high")
+                break;
+    }
+        
+        
 }
 
 function updateFavButtons(cityName){
@@ -30,6 +60,39 @@ function updateFavButtons(cityName){
 }
 
 function fillInCardDeck(data){
+    // The data coming back is in an array of 8 readings per day time 5 days.
+    // Only the midnight reading is for the whole day, so we'll only take that reading.
+
+    var cardDT 
+    var cardDate
+    var cardDateElement
+    var cardTempElement
+    var cardWindElement
+    var cardHumidityElement
+    var cardIconElement
+    var forecastArrayAdjustment = 0
+
+    for (day=0;day<5;day++){
+        //looping through each card in the deck
+        debugger
+        cardDT = new Date(data.list[forecastArrayAdjustment].dt_txt); 
+        cardDate = ("(" + (cardDT.getMonth()+1) + "/" + cardDT.getDate() + "/" + cardDT.getFullYear() + ")");  
+        
+        cardDateElement = document.querySelector("#day" + (day + 1).toString() + "Date")
+        cardDateElement.textContent = cardDate
+        cardTempElement = document.querySelector("#day" + (day + 1).toString() + "Temp")
+        cardTempElement.textContent = data.list[forecastArrayAdjustment].main.temp
+        cardWindElement = document.querySelector("#day" + (day + 1).toString() + "Wind")
+        cardWindElement.textContent = data.list[forecastArrayAdjustment].wind.speed
+        cardHumidityElement = document.querySelector("#day" + (day + 1).toString() + "Humidity")
+        cardHumidityElement.textContent = data.list[forecastArrayAdjustment].main.humidity
+        cardIconElement = document.querySelector("#day" + (day + 1).toString() + "Icon")
+        cardIconElement.setAttribute("src","http://openweathermap.org/img/wn/" + data.list[forecastArrayAdjustment].weather[0].icon + "@2x.png")
+
+        // Looks weird, but this adjusts how we pull the data from the array that comes back
+        // it's based on the first entry that happens on each day at midnight
+        forecastArrayAdjustment = ((day + 1) * 7) + 1
+    }
 
 }
 
@@ -65,8 +128,7 @@ function weatherFetch(cityName) {
         if (res1.ok) {
             res1.json().then(function(data) {
             // Calling 2nd Fetch to get current day data including UV Index
-                debugger
-
+                //debugger
                 var cityLat = data.city.coord.lat
                 var cityLon = data.city.coord.lon
                 apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=minutely,hourly,daily,alerts&units=imperial&appid=" + openWeatherAPI
@@ -77,13 +139,14 @@ function weatherFetch(cityName) {
                         res2.json().then(function(currentDay){
                             debugger
                             fillInMainCard(currentDay)
-                            apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + openWeatherAPI
+                            //apiUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + cityName + "&cnt=5&units=imperial&appid=" + openWeatherAPI
+                            apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&cnt=40&exclude=minutely,hourly,alerts&units=imperial&appid=" + openWeatherAPI
                         //Calling 3rd Fetch to get 5 day forecast to fill in lower card deck
                         return fetch(apiUrl)
                             .then (function(res3){
                             if (res3.ok){
                                 //This gives us the 5 day forecast data we need
-                                res2.json().then(function(forecast){
+                                res3.json().then(function(forecast){
                                     debugger
                                     fillInCardDeck(forecast)
                                     updateFavButtons(cityName) 
